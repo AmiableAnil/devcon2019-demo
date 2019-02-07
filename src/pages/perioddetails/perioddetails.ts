efe372287e830b1e0e03b5e679e5354b2e3f2fa0
diptesh6501 <diptesh1993@gmail.com>
2019-02-08 00:51:50 +0530

    added player integration

--- src/pages/perioddetails/perioddetails.ts
+++ src/pages/perioddetails/perioddetails.ts
@@ -1,6 +1,6 @@
 import { ReportAlertComponent } from './../../components/report-alert/report-alert';
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, ModalController, PopoverController, Events, Platform, ViewController, IonicApp } from 'ionic-angular';
+import { Component, NgZone } from '@angular/core';
+import { IonicPage, NavController, NavParams, ModalController, PopoverController, Events, Platform, ViewController, IonicApp, LoadingController } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
 //import { AttendancePage } from '../attendance/attendance';
 //import { ReportAlertComponent } from '../../components/report-alert/report-alert';
@@ -36,12 +36,19 @@ export class PerioddetailsPage {
   topicInfo;
   showStartClass = false;
   backButtonFunc = undefined;
+  practise: any;
+  quiz: any;
+  recall: any;
+  revise: any;
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public httpClient: HttpClient, private modalCtrl: ModalController, private popoverCtrl: PopoverController,
     public events: Events,
     private platform: Platform,
     private viewCtrl: ViewController,
-    private ionicApp: IonicApp) {
+    private ionicApp: IonicApp,
+    private loadingCtrl: LoadingController,
+    private zone: NgZone) {
+    this.getApiResponse();
     // this.data = this.navParams.get('data');
 
     // const date = this.navParams.get('date');
@@ -153,7 +160,27 @@ export class PerioddetailsPage {
       return avg;
     }
   }
-  // this.http.g
+  getApiResponse() {
+    const loader = this.getLoader();
+    loader.present();
+    this.httpClient.get('https://dev.ekstep.in/api/dialcode/v3/question/read/geography').subscribe((response) => {
+      console.log('response is =>', response);
+      let data = response['result'].content;
+      console.log('data is =>', data);
+      data.forEach(item => {
+        if (item.usecase === 'assess') {
+          this.quiz = item;
+        } else if (item.usecase === 'practice') {
+          this.practise = item;
+        } else if (item.usecase === 'recall') {
+          this.recall = item;
+        } else if (item.usecase === 'revise') {
+          this.revise = item;
+        }
+      });
+      loader.dismiss();
+    })
+  }
 
   // openHeatMap() {
   //   const popover = this.popoverCtrl.create(ReportAlertComponent, {
@@ -229,6 +256,24 @@ export class PerioddetailsPage {
     }, 10);
 
   }
-}
+  alert(contentData) {
+    console.log('content data =>', contentData);
+    this.zone.run(() => {
+      console.log('asasasasa' + (<any>window).geniecanvas);
+      const request: any = {};
+      request.streaming = true;
+      // const content ='{"contentAccess":[{"contentLearnerState":{},"status":1}],"contentData":{contentData"appIcon":"https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_21264270761184460811673/artifact/0288d78758c4463dd40892d5a460edf4_1478578597914.thumb.jpeg","artifactUrl":"https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_21264270761184460811673/artifact/1543303326995_do_21264270761184460811673.zip","attributions":["DIKSHA"],"audience":["Learner"],"board":"State (Tamil Nadu)","channel":"in.ekstep","contentDisposition":"inline","contentEncoding":"gzip","contentType":"Resource","createdBy":"b9a3972c-a2ba-46fa-9279-59ce21957a83","createdOn":"2018-11-27T06:14:29.024+0000","creator":"DemoCreator Creator","description":"Grade 4 Math Activity","downloadUrl":"https://ntpstagingall.blob.core.windows.net/ntp-content-staging/ecar_files/do_21264270761184460811673/mathematics-activity-class-4_1543303328064_do_21264270761184460811673_2.0.ecar","framework":"NCF","gradeLevel":["Class 4"],"identifier":"do_21264270761184460811673","language":["English"],"lastPublishedOn":"2018-11-27T07:22:07.149+0000","medium":"English","mimeType":"application/vnd.ekstep.ecml-archive","name":"Mathematics Activity - Class 4","osId":"org.ekstep.quiz.app","owner":"DemoCreator Creator","pkgVersion":"2.0","previewUrl":"https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/ecml/do_21264270761184460811673-latest","resourceType":"Test","size":"562094.0","status":"Live","streamingUrl":"https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/ecml/do_21264270761184460811673-latest","subject":"Mathematics","variants":{"spine":{"ecarUrl":"https://ntpstagingall.blob.core.windows.net/ntp-content-staging/ecar_files/do_21264270761184460811673/mathematics-activity-class-4_1543303328175_do_21264270761184460811673_2.0_spine.ecar","size":12590}},"versionKey":"1543303326455","downloadable":false,"contentAccess":[{"contentLearnerState":{},"status":1}],"contentMarker":[{"contentId":"do_21264270761184460811673","extraInfoMap":{"hierarchyInfo":[]},"marker":1,"uid":"89bafcca-e2a4-4494-bc2c-1d179ef0ab03"}]},"contentFeedback":[],"contentMarker":[{"contentId":"do_21264270761184460811673","extraInfoMap":{"hierarchyInfo":[]},"marker":1,"uid":"89bafcca-e2a4-4494-bc2c-1d179ef0ab03"}],"contentType":"resource","identifier":"do_21264270761184460811673","isAvailableLocally":false,"isUpdateAvailable":false,"lastUpdatedTime":0,"mimeType":"application/vnd.ekstep.ecml-archive","referenceCount":1}';
+      const content = { "contentAccess": [{ "contentLearnerState": {}, "status": 1 }], "contentData": contentData, "contentFeedback": [], "contentMarker": [{ "contentId": contentData.identifier, "extraInfoMap": { "hierarchyInfo": [] }, "marker": 1, "uid": "89bafcca-e2a4-4494-bc2c-1d179ef0ab03" }], "contentType": "resource", "identifier": "do_21264270761184460811673", "isAvailableLocally": false, "isUpdateAvailable": false, "lastUpdatedTime": 0, "mimeType": "application/vnd.ekstep.ecml-archive", "referenceCount": 1 };
+      console.log('content is =>', JSON.stringify(content));
 
+      (<any>window).geniecanvas.play(JSON.stringify(content), JSON.stringify(request));
+    });
+  }
+  getLoader(): any {
+    return this.loadingCtrl.create({
+      duration: 30000,
+      spinner: 'crescent'
+    });
+  }
+}
 
